abc2a4ceb4520408ae20c02948a372d9c543ec68
Debakanta <debakantapradhan5@gmail.com>
2024-01-26 11:32:59 +0530

    project initialized

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandller.java
@@ -0,0 +1,29 @@
+package com.example.demo.exception;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandller {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+		Map<String, String> errors = new LinkedHashMap<>();
+		ex.getBindingResult().getAllErrors().forEach(err->{
+			String feildName = ((FieldError)err).getField();
+			String errorMessage = err.getDefaultMessage();
+			errors.put(feildName, errorMessage);
+		});
+		return new ResponseEntity<Map<String,String>>(errors,HttpStatus.BAD_REQUEST);
+	}
+	
+//	@ExceptionHandler(EmployeeNotFoundException.class)
+//	public ResponseEntity<T>
+}
